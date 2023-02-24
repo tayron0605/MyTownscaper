8da6792e4f204d8d7152d4dbd129009590f97040
tayron0605 <1652271127@qq.com>
2023-02-24 10:24:52 +0800

    init

---
+++ assets/Script/UIMap.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('UIMap')
+export class UIMap extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
